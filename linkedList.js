e1f55bb688b901595c7edd3df60216eb71d22081
Scott Wright <scott.wright.uk@gmail.com>
2023-11-09 15:28:20 +0000

    add: prepend method

--- linkedList.js
+++ linkedList.js
@@ -13,26 +13,42 @@ class LinkedList {
         this.length = 0; // The total number of nodes in the list
     }
 
-        // Method to add a new node to the END of the list
-        append(value) {
-            const newNode = new Node(value); // Create a new node with the given value
-    
-            // If the list is empty, the new node becomes the head of the list
-            if (this.length === 0) {
-                this.head = newNode;
-            } else {
-                // If the list is not empty, we need to find the last node
-                let currentNode = this.head; // Start at the head of the list
-                // Traverse the list until we find the last node (which points to null)
-                while (currentNode.nextNode) {
-                    currentNode = currentNode.nextNode; // Move to the next node
-                }
-                // Once the last node is found, set its nextNode to the new node
-                currentNode.nextNode = newNode; // Ensure this matches your Node class property
+    // Method to add a new node to the END of the list
+    append(value) {
+        const newNode = new Node(value); // Create a new node with the given value
+
+        // If the list is empty, the new node becomes the head of the list
+        if (this.length === 0) {
+            this.head = newNode;
+        } else {
+            // If the list is not empty, we need to find the last node
+            let currentNode = this.head; // Start at the head of the list
+            // Traverse the list until we find the last node (which points to null)
+            while (currentNode.nextNode) {
+                currentNode = currentNode.nextNode; // Move to the next node
             }
-            // Increment the size of the list to reflect the addition of the new node
-            this.length++;
+            // Once the last node is found, set its nextNode to the new node
+            currentNode.nextNode = newNode;
         }
+        // Increment the size of the list to reflect the addition of the new node
+        this.length++;
+    }
+
+    // Method to add a new node to the START of the list
+    prepend(value) {
+        const newNode = new Node(value);
+
+        // If the list is empty, the new node becomes the head of the list
+        if (this.length === 0) {
+            this.head = newNode;
+        } else {
+            // If the list is not empty
+            newNode.nextNode = this.head; // Point the new node to the current head
+            this.head = newNode; // The new node is now the new head of the list
+        }
+        // Increment the size of the list to reflect the addition of the new node
+        this.length++;
+    }
 }
 
 // testing area
@@ -42,4 +58,5 @@ testList.append(1);
 testList.append(2);
 testList.append(3);
 testList.append(4);
+testList.prepend(0);
 console.log(testList);
