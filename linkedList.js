bafb1ab992bc7aed341ba887bfc6be61d6bac1b7
Scott Wright <scott.wright.uk@gmail.com>
2023-11-09 15:25:21 +0000

    add: append method

--- linkedList.js
+++ linkedList.js
@@ -12,9 +12,34 @@ class LinkedList {
         this.head = null; // The first node in the list
         this.length = 0; // The total number of nodes in the list
     }
+
+        // Method to add a new node to the END of the list
+        append(value) {
+            const newNode = new Node(value); // Create a new node with the given value
+    
+            // If the list is empty, the new node becomes the head of the list
+            if (this.length === 0) {
+                this.head = newNode;
+            } else {
+                // If the list is not empty, we need to find the last node
+                let currentNode = this.head; // Start at the head of the list
+                // Traverse the list until we find the last node (which points to null)
+                while (currentNode.nextNode) {
+                    currentNode = currentNode.nextNode; // Move to the next node
+                }
+                // Once the last node is found, set its nextNode to the new node
+                currentNode.nextNode = newNode; // Ensure this matches your Node class property
+            }
+            // Increment the size of the list to reflect the addition of the new node
+            this.length++;
+        }
 }
 
 // testing area
 const testList = new LinkedList();
 
+testList.append(1);
+testList.append(2);
+testList.append(3);
+testList.append(4);
 console.log(testList);
